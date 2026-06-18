261508da724cb0c5435d12db1249497794a6333d
Vishnubbbbb <133390258+Vishnubbbbb@users.noreply.github.com>
2026-06-17 20:32:23 -0700

    created the new react and login updated

---
+++ Lab6/lab6-react/src/Login.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function Login(){
+    const [loginFormData,setLoginFormData] = useState("{username: '', password: ''}")
+
+    const loginSubmit = () => {
+        //alert(loginFormData.username);
+    }
+
+    return (
+        <div className="loginBox">
+            <h1>ACME University Login</h1>
+
+            <form onSubmit={loginSubmit}>
+                <p>Username</p>
+                <input id="username" name="username" type="text" placeholder="Username" 
+                value={loginFormData.username} 
+                onChange={(e)=>setLoginFormData((x)=>({...x, username:e.target.value}))}
+                required />
+
+                <p>Password</p>
+                <input type="password" id="password" name="password" placeholder="Password"
+                value={loginFormData.password} 
+                onChange={(e)=>setLoginFormData((x)=>({...x, password:e.target.value}))}
+                required />
+        
+                <button type="submit">Sign in</button>
+            </form>
+
+            <div className="hintBox">
+                <p>Hint for testing:</p>
+                <p>Student Login: cnorris / 123</p>
+                <p>Teacher Login: ahepworth / 123</p>
+            </div>
+        </div>
+    )
+}
+
+export default Login
+
+
+
